1d36d327c64d58be330e882730817422dfc8597e
rogertht <43129529+rogertht@users.noreply.github.com>
2019-11-21 00:12:35 +0200

    Egerti baasrakendus

---
+++ src/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import mapboxgl from 'mapbox-gl';
+import './App.css';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+
+class Application extends React.Component {
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      lng: 24.7731,
+      lat: 59.4389,
+      zoom: 12
+    };
+  }
+
+  componentDidMount() {
+    const { lng, lat, zoom } = this.state;
+
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [lng, lat],
+      zoom
+    });
+
+    map.on('move', () => {
+      const { lng, lat } = map.getCenter();
+
+      this.setState({
+        lng: lng.toFixed(4),
+        lat: lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
+    });
+  }
+
+  render() {
+    const { lng, lat, zoom } = this.state;
+
+    return (
+      <div>
+        <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
+          <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
+        </div>
+        <div ref={el => this.mapContainer = el} className="absolute top right left bottom" />
+        <div className="center-screen">
+            <div className="marker"></div>
+            <div className="pin2"></div>
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Application />, document.getElementById('app'));
